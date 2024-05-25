52eb033d4e69b56bd636f02d9ab6ea2e5dfd15a6
clientsmaker <interiorclientsmaker@gmail.com>
2024-05-25 12:05:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline text-center align-middle justify-center">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
